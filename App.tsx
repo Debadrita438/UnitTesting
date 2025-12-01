5bfb92dc867543c28c97ef180c10d8c93781b7aa
debadrita438 <bristibandyopadhyay@gmail.com>
2025-12-01 12:43:28 +0530

    initial project setup and testing

--- App.tsx
+++ App.tsx
@@ -1,45 +1,70 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+// import { NewAppScreen } from '@react-native/new-app-screen';
+// import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+// import {
+//   SafeAreaProvider,
+//   useSafeAreaInsets,
+// } from 'react-native-safe-area-context';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+// function App() {
+//   const isDarkMode = useColorScheme() === 'dark';
 
+//   return (
+//     <SafeAreaProvider>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <AppContent />
+//     </SafeAreaProvider>
+//   );
+// }
+
+// function AppContent() {
+//   const safeAreaInsets = useSafeAreaInsets();
+
+//   return (
+//     <View style={styles.container}>
+//       <NewAppScreen
+//         templateFileName="App.tsx"
+//         safeAreaInsets={safeAreaInsets}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
+
+// export default App;
+
+import { View, Text, Pressable } from 'react-native';
+import React, { useState } from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0);
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+      }}
+    >
+      <Text testID="count">{count}</Text>
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Pressable onPress={() => setCount(pre => pre + 1)}>
+        <Text>Press me</Text>
+      </Pressable>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+};
 
 export default App;
--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -3,11 +3,23 @@
  */
 
 import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
+import { render, screen, userEvent } from '@testing-library/react-native';
 
-test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+describe('App', () => {
+  it('renders correctly', () => {
+    render(<App />);
+
+    expect(screen.getByTestId('count')).toBeTruthy();
+  });
+
+  it('renders the number of times the button has been pressed', async () => {
+    render(<App />);
+    const user = userEvent.setup();
+
+    const count = screen.getByTestId('count');
+    expect(count.children[0]).toBe('0');
+    await user.press(screen.getByText('Press me'));
+    expect(count.children[0]).toBe('1');
   });
 });
